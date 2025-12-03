2af007ac02ea3960ff282f5f70be3122409dcde7
NEOFIN-JPinto <joao.pinto@neofin.co.uk>
2025-12-03 14:43:05 +0000

    CH2L4 Term Frequency

--- cli/inverted_index.py
+++ cli/inverted_index.py
@@ -1,25 +1,38 @@
 import pickle
 import os
 from utils import *
-
+from collections import Counter
 class InvertedIndex:
     def __init__(self):
         self.index = {}
         self.docmap = {}
+        self.term_frequencies = {}
     
     def __add_document(self, doc_id, text):
         tokens = clean_text(text)
+        c = Counter()
         for token in tokens:
+            c[token] += 1
             if token not in self.index:
                 self.index[token] = [doc_id]
             else:
                 self.index[token].append(doc_id)
+        
+        self.term_frequencies[doc_id]=c
             
     def get_documents(self, term):
         if term in self.index:
             return sorted(self.index[term])
         else:
             return []
+        
+    def get_tf(self,doc_id,term):
+        if doc_id in self.term_frequencies:
+            if term in self.term_frequencies[doc_id]:
+                return self.term_frequencies[doc_id][term]
+            
+        return 0
+
     
     def build(self,movies):
         for movie in movies:
@@ -36,6 +49,8 @@ class InvertedIndex:
             pickle.dump(self.index,f)
         with open("./cache/docmap.pkl", 'wb') as f:  
             pickle.dump(self.docmap,f)
+        with open("./cache/term_frequencies.pkl", 'wb') as f:  
+            pickle.dump(self.term_frequencies,f)
             
     def load(self):
         def load_pickle(path):
@@ -44,12 +59,13 @@ class InvertedIndex:
             file.close() 
             
             return pkl     
+        
         try:
             self.index = load_pickle("./cache/index.pkl")
             self.docmap = load_pickle("./cache/docmap.pkl")
+            self.term_frequencies = load_pickle("./cache/term_frequencies.pkl")
         except:
             raise ValueError("Pickle Objects do not exist. Use build method to generate before loading.")
 
     
             
-        
--- cli/keyword_search_cli.py
+++ cli/keyword_search_cli.py
@@ -8,26 +8,28 @@ from inverted_index import InvertedIndex
 from utils import *
 
 
-
-
 def main() -> None:
     parser = argparse.ArgumentParser(description="Keyword Search CLI")
     subparsers = parser.add_subparsers(dest="command", help="Available commands")
 
+    build_parser = subparsers.add_parser("build", help="Build Inverted Index table")
+    tf_parser = subparsers.add_parser("tf", help="Look for term frequency in document")
+    tf_parser.add_argument("doc_id", type=str, help="Search query")
+    tf_parser.add_argument("token", type=str, help="Search query")
+
     search_parser = subparsers.add_parser("search", help="Search movies using BM25")
     search_parser.add_argument("query", type=str, help="Search query")
 
-    build_parser = subparsers.add_parser("build", help="Build Inverted Index table")
     args = parser.parse_args()
     
     #Loading movies
     path = r'./data/movies.json'
     movies_dict = get_movies_dict(path)
+    inverted_index = InvertedIndex()           
 
     match args.command:
         case "search":
             print(f"Searching for: {args.query}")
-            inverted_index = InvertedIndex()           
             try:
                 inverted_index.load()
             except:
@@ -48,8 +50,20 @@ def main() -> None:
             inverted_index = InvertedIndex()
             inverted_index.build(movies_dict['movies'])
             inverted_index.save()
-            docs = inverted_index.get_documents('merida')
-            print(f"First document for token 'merida' = {docs[0]}")
+
+        case "tf":
+
+            doc_id, token = int(args.doc_id), args.token
+            term = clean_text(token)[0]
+            try:
+                inverted_index.load()
+            except:
+                print("Error loading index")
+                exit(1)
+
+            freq = inverted_index.get_tf(doc_id,term)
+            print(f"Document {doc_id} contains the term {term} {freq} times")
+            
             
         case _:
             parser.print_help()
